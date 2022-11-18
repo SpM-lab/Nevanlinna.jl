b9b7780fd446d8deb1689b261cedeb55e30c1084
kosuke <kosuke.moco.leon@gmail.com>
2022-11-18 11:06:44 +0900

    c++ moved

--- flex_src/basis.jl
+++
@@ -1,4 +0,0 @@
-getfnw(basis::FiniteTempBasisSet) = length(basis.smpl_wn_f.sampling_points)
-getbnw(basis::FiniteTempBasisSet) = length(basis.smpl_wn_b.sampling_points)
-getntau(basis::FiniteTempBasisSet) = length(basis.smpl_tau_f.sampling_points)
-getnl(basis::FiniteTempBasisSet) = length(basis.basis_f)
--- flex_src/exports.jl
+++
@@ -1,5 +0,0 @@
-#export FiniteTempBasis, MatsubaraSampling, TauSampling
-#export fermion, boson
-#export evaluate, fit
-export IRBasisSet
-export LatticeModel, Variables, SCFSolution
